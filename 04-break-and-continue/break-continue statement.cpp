f3cb337e1c105a31a11a401523f3f5bd3b759098
Dilanjan Sulochana <dilanjansulochana568@gmail.com>
2026-06-23 00:40:33 +0530

    Add Break & Continue
    Uploaded break practical and couninue practical.

---
+++ 04-break-and-continue/break-continue statement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for( int i=0;i<=10;i++)
+    {
+        if(i==5)
+    {
+        continue;
+    }
+    else if(i==7)
+    {
+        break;
+    }
+    cout<<i<<"\n";
+    }
+}
